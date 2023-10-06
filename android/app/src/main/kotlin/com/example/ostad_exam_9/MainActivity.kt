39668c1b03b003e9440366979031bfe71d4621ac
HafizSp <hafizsp45@gmail.com>
2023-10-06 23:21:13 +0600

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/ostad_exam_9/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.ostad_exam_9
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,96 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  String selectedSize = '';
+
+  void _showSnackbar(BuildContext context, String size) {
+    final snackBar = SnackBar(
+      content: Text('Selected Size: $size'),
+      duration: const Duration(seconds: 1),
+    );
+    ScaffoldMessenger.of(context).showSnackBar(snackBar);
+  }
+
+  void _changeButtonColor(String size) {
+    setState(() {
+      selectedSize = size;
+    });
+  }
+
+  Widget _buildButton(BuildContext context, String size) {
+    return ElevatedButton(
+      onPressed: () {
+        _changeButtonColor(size);
+        _showSnackbar(context, size);
+      },
+      style: ButtonStyle(
+        backgroundColor: MaterialStateProperty.all(
+          size == selectedSize ? Colors.green : Colors.blue,
+        ),
+      ),
+      child: Text(size),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Size Selector'),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(16),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  _buildButton(context, "S"),
+                  const SizedBox(width: 10.0),
+                  _buildButton(context, "M"),
+                  const SizedBox(width: 10.0),
+                  _buildButton(context, "L"),
+                  const SizedBox(width: 10.0),
+                  _buildButton(context, "XL"),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  _buildButton(context, "XXL"),
+                  const SizedBox(width: 10.0),
+                  _buildButton(context, "XXXL"),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
